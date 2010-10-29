e6dbd7a8b22c457685583bd07e891e109857a8ea
blfrie <blfrie@a9c4d81f-ea81-26b5-ec18-9d91c9fa7bcf>
2010-10-29 03:27:01 +0000

    New project (again)

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=libparkville.a
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/libparkville.a
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=parkville.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/parkville.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=libparkville.a
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/libparkville.a
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=parkville.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/parkville.tar
